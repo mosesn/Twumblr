00f6be55dbea48f0487edf21e54beac0ed83a2c3
Moses Nakamura <nnythm@gmail.com>
2011-10-02 00:41:19 -0400

    removed static shit

--- twumblr/__init__.py
+++ twumblr/__init__.py
@@ -9,14 +9,9 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     initialize_sql(engine)
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'twumblr:static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_view('twumblr.views.my_view',
-                    route_name='home',
-                    renderer='templates/mytemplate.pt')
+#    config.add_static_view('static', 'twumblr:static', cache_max_age=3600)
     config.add_route('oauth', '/tumblr_auth')
     config.add_view('twumblr.views.obtain_oauth',
                     route_name='oauth',
                     renderer='templates/oauthtemp.pt')
     return config.make_wsgi_app()
-
--- twumblr/views.py
+++ twumblr/views.py
@@ -1,10 +1,2 @@
-from twumblr.models import DBSession
-from twumblr.models import MyModel
-
-def my_view(request):
-    dbsession = DBSession()
-    root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
-    return {'root':root, 'project':'twumblr'}
-
 def obtain_oauth(request):
     return {"data" : request.params.items()}
