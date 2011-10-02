52f4d12e467c6231675aa06539b3eede5482e7a8
dannenberg <dannenberg.matt@gmail.com>
2011-10-02 06:28:48 -0400

    fuck fuck fuck fuck

--- twumblr/__init__.py
+++ twumblr/__init__.py
@@ -1,16 +1,31 @@
 from pyramid.config import Configurator
 
+from pyramid.session import UnencryptedCookieSessionFactoryConfig
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+
+    my_session_factory = UnencryptedCookieSessionFactoryConfig('itsaseekreet')
+    config = Configurator(settings=settings, session_factory = my_session_factory)
 #    config.add_static_view('static', 'twumblr:static', cache_max_age=3600)
-    config.add_route('oauth', '/tumblr_auth')
     config.add_route('home', '/')
+    config.add_view('twumblr.views.get_twitter',
+                    route_name='home',
+                    renderer='templates/get_twitter.pt')
+    
+    config.add_route('gen_url', '/gen_url')
+    config.add_view('twumblr.views.gen_url',
+                    route_name='gen_url',
+                    renderer='templates/gen_url.pt')
+    
+    config.add_route('remove_twitter', '/remove_twitter')
+    config.add_view('twumblr.views.remove_twitter',
+                    route_name='remove_twitter',
+                    renderer='templates/remove_twitter.pt')
+    
+    config.add_route('oauth', '/tumblr_auth')
     config.add_view('twumblr.views.obtain_oauth',
                     route_name='oauth',
                     renderer='templates/oauthtemp.pt')
-    config.add_view('twumblr.views.gen_url',
-                    route_name='home',
-                    renderer='templates/gen_url.pt')
     return config.make_wsgi_app()
--- twumblr/views.py
+++ twumblr/views.py
@@ -2,30 +2,48 @@ from tumblr.oauth import TumblrOAuthClient
 import json
 from pymongo import Connection
 
-def gen_url(request):
-    consumer_key = 'BmyWZMbAzcK9Y7mEQKTgf1JI4icFlXvfxxkfIzuG9nFFVJfg9Q'
-    consumer_secret = 'p5ohAI2hT7tSwjVCI0HA8oTpOYAvc3m6tIPAXJGNXkur6PgQdT'
+consumer_key = 'BmyWZMbAzcK9Y7mEQKTgf1JI4icFlXvfxxkfIzuG9nFFVJfg9Q'
+consumer_secret = 'p5ohAI2hT7tSwjVCI0HA8oTpOYAvc3m6tIPAXJGNXkur6PgQdT'
 
-    tumblr_oauth = TumblrOAuthClient(consumer_key, consumer_secret)
-    #authorize_url = tumblr_oauth.get_authorize_url()
-    return {"url": "fuck"}
+tumblr_oauth = TumblrOAuthClient(consumer_key, consumer_secret)
+
+def get_twitter(request):
+    return {}
+
+def gen_url(request):
+    session = request.session
+    authorize_url = tumblr_oauth.get_authorize_url()
+    session["secret"] = tumblr_oauth.request_token["oauth_token_secret"]
+    session["token"] = tumblr_oauth.request_token["oauth_token"]
+    session["twitter"] = request.params["twitter"]
+    return {"url":authorize_url} 
+
+def remove_twitter(request):
+    fp = open("/home/dotcloud/environment.json")
+    dbloc = json.load(fp)["DOTCLOUD_DATA_MONGODB_URL"]
+    fp.close()
+    conn = Connection(dbloc)
+    coll = conn.db.users
+
+    handle = request.params["twitter"]
+    coll.remove({"twitter":handle})
 
 def obtain_oauth(request):
-#    fp = open("/home/dotcloud/environment.json")
-#    dbloc = json.load(fp.read())["DOTCLOUD_DATA_MONGODB_URL"]
-#    fp.close()
-#    conn = Connection(dbloc)
-#    coll = conn.db.users
-
-#    consumer_key = 'BmyWZMbAzcK9Y7mEQKTgf1JI4icFlXvfxxkfIzuG9nFFVJfg9Q'
-#    consumer_secret = 'p5ohAI2hT7tSwjVCI0HA8oTpOYAvc3m6tIPAXJGNXkur6PgQdT'
-
-#    tumblr_oauth = TumblrOAuthClient(consumer_key, consumer_secret)
+    fp = open("/home/dotcloud/environment.json")
+    dbloc = json.load(fp)["DOTCLOUD_DATA_MONGODB_URL"]
+    fp.close()
+    conn = Connection(dbloc)
+    coll = conn.db.users
 
     oauth_verifier = request.params["oauth_verifier"]
-#    access_token = tumblr_oauth.get_access_token(oauth_verifier)
-#    coll.insert({"key" : access_token.key, "secret" : access_token.secret})
+
+    session = request.session
+    tumblr_oauth.request_token = {"oauth_token_secret":session["secret"],"oauth_token":session["token"]}
+    access_token = tumblr_oauth.get_access_token(oauth_verifier)
+
+    coll.insert({"key" : access_token.key, "secret" : access_token.secret, "twitter":session["twitter"]})
+    return coll.find_one( {"key" : access_token.key, "secret" : access_token.secret})
 #    print "Access key:", access_token.key
 #    print "Access Secret:", access_token.secret
     
-    return {}
+#    return {}
