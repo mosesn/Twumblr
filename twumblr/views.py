ba04b81c41f11732c9d2755604582a24206fd0c5
dannenberg <dannenberg.matt@gmail.com>
2011-10-02 03:20:20 -0400

    super minimal

--- twumblr/views.py
+++ twumblr/views.py
@@ -3,12 +3,12 @@ import json
 from pymongo import Connection
 
 def gen_url(request):
-    consumer_key = 'BmyWZMbAzcK9Y7mEQKTgf1JI4icFlXvfxxkfIzuG9nFFVJfg9Q'
-    consumer_secret = 'p5ohAI2hT7tSwjVCI0HA8oTpOYAvc3m6tIPAXJGNXkur6PgQdT'
+#    consumer_key = 'BmyWZMbAzcK9Y7mEQKTgf1JI4icFlXvfxxkfIzuG9nFFVJfg9Q'
+#    consumer_secret = 'p5ohAI2hT7tSwjVCI0HA8oTpOYAvc3m6tIPAXJGNXkur6PgQdT'
 
-    tumblr_oauth = TumblrOAuthClient(consumer_key, consumer_secret)
-    authorize_url = tumblr_oauth.get_authorize_url()
-    return {"url":authorize_url}
+#    tumblr_oauth = TumblrOAuthClient(consumer_key, consumer_secret)
+#    authorize_url = tumblr_oauth.get_authorize_url()
+    return {"url": "fuck"}
 
 def obtain_oauth(request):
 #    fp = open("/home/dotcloud/environment.json")
