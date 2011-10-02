c4e2431ad49ee59b5c718c475c691252b45961c2
dannenberg <dannenberg.matt@gmail.com>
2011-10-02 01:57:32 -0400

    db test

--- oauth.py
+++ oauth.py
@@ -7,3 +7,8 @@ tumblr_oauth = TumblrOAuthClient(consumer_key, consumer_secret)
 authorize_url = tumblr_oauth.get_authorize_url()
 
 print "visit: %s" % authorize_url
+
+oauth_verifier = raw_input('What is the oauth_verifier?')
+access_token = tumblr_oauth.get_access_token(oauth_verifier)
+print "Access key:", access_token.key
+print "Access Secret:", access_token.secret
--- twumblr/views.py
+++ twumblr/views.py
@@ -1,2 +1,17 @@
+import json
+from pymongo import Connection
+
+
 def obtain_oauth(request):
-    return {"items" : request.params.items()}
+    fp = open("/home/dotcloud/environment.json")
+    dbloc = json.load(fp.read())["DOTCLOUD_DATA_MONGODB_URL"]
+    fp.close()
+    conn = Connection(dbloc)
+    coll = conn.db.users
+
+    oauth_verifier = request.params["oauth_verifier"]
+    access_token = tumblr_oauth.get_access_token(oauth_verifier)
+    coll.insert({"key" : access_token.key, "secret" : access_token.secret})
+    print "Access key:", access_token.key
+    print "Access Secret:", access_token.secret
+    
