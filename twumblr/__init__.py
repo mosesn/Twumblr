05ea1b54882180940fcc697a51c0fcda334c2ad7
dannenberg <dannenberg.matt@gmail.com>
2011-10-02 03:18:32 -0400

    added new template and stuff

--- twumblr/__init__.py
+++ twumblr/__init__.py
@@ -6,7 +6,11 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
 #    config.add_static_view('static', 'twumblr:static', cache_max_age=3600)
     config.add_route('oauth', '/tumblr_auth')
+    config.add_route('home', '/')
     config.add_view('twumblr.views.obtain_oauth',
                     route_name='oauth',
                     renderer='templates/oauthtemp.pt')
+    config.add_view('twumblr.views.gen_url',
+                    route_name='home',
+                    renderer='templates/gen_url.pt')
     return config.make_wsgi_app()
