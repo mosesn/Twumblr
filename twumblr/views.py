b903842b03d7e5d53e30c86ad0b6c5152a76ad64
dannenberg <dannenberg.matt@gmail.com>
2011-10-02 03:22:30 -0400

    one more thing

--- twumblr/views.py
+++ twumblr/views.py
@@ -7,7 +7,7 @@ def gen_url(request):
     consumer_secret = 'p5ohAI2hT7tSwjVCI0HA8oTpOYAvc3m6tIPAXJGNXkur6PgQdT'
 
     tumblr_oauth = TumblrOAuthClient(consumer_key, consumer_secret)
-#    authorize_url = tumblr_oauth.get_authorize_url()
+    authorize_url = tumblr_oauth.get_authorize_url()
     return {"url": "fuck"}
 
 def obtain_oauth(request):
